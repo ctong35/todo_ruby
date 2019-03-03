b496e23062ed1cd6cfec01f2c9d5515866f720a0
Chris Tong <ctong35@bloomberg.net>
2019-03-03 20:36:00 +0000

    add update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -10,6 +10,17 @@ class TodoController < ApplicationController
     def new
         
     end
+    def update
+        t = Todo.find_by_id(params['id'])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    
     def create
         t = Todo.new
         t.description = params['description']
